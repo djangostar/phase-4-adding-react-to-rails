9c441c072ce6cba0c81cbcee4ece64d5dc23c69f
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-09-20 10:02:46 -0700

    Done.

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,14 @@
 import { useEffect } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   useEffect(() => {
     fetch("/movies")
-      .then((res) => res.json())
+      .then((r) => r.json())
       .then((movies) => console.log(movies));
   }, []);
 
-  return <h1>Hello From React!</h1>;
+  return <h1>Hello from React!</h1>;
 }
 
 export default App;
